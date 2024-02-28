0dada020ae67ad5a5954dcb29a5177a9a8ff28be
TawerV10 <108547860+TawerV10@users.noreply.github.com>
2024-02-29 00:23:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if user_token != SECRET_TOKEN:
     st.stop()
 
 # st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-st.set_page_config(page_title="Sales Dashboard")
+# st.set_page_config(page_title="Sales Dashboard")
 
 
 @st.cache_data
